3b7ed04f9bf24afc606562a461b8516615ad4e02
ololadetemi <ololadetoluwalase@gmail.com>
2024-09-11 12:43:21 +0100

    built lab schema with file upload capabilities

--- models/labModel.js
+++ models/labModel.js
@@ -5,5 +5,30 @@ const labSchema = new mongoose.Schema({
     patientName: {
         type: mongoose.Schema.Types.ObjectId, ref: 'Patient',
         required: true
+    },
+    labTechnician: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'User',
+        required: true
+    },
+    testName: {
+        type: String,
+        required: true
+    },
+    result: {
+        type: String  //this is for storing results in text
+    },
+    resultFile: { 
+        type: String  //file path or url of the uploaded result
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    notes: {
+        type: String  //additional notes by the lab technician
     }
-})
+});
+
+const Lab = mongoose.model('lab', labSchema);
+
+module.exports = Lab;
