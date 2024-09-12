6edeb7d46f8ca91bb5b78368c48ca120a15e35f3
ololadetemi <ololadetoluwalase@gmail.com>
2024-09-13 00:21:44 +0100

    doctor controller

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,3 +1,11 @@
 const Patient = require('../models/patientModel')
 const Appointment = require('../models/appointmentModel');
 
+exports.getPatientRecordsb= async(req, res) => {
+    try{
+        const patientRecords = await Patient.find();
+        res.status(200).json(patientRecords);
+    } catch (error) {
+        res.status(500).json({ meessage: 'error fetching patient records', error});
+    }
+}
