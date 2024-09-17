cf299ed76a726ba13bf75261ab9a1f23014cccd1
ololadetemi <ololadetoluwalase@gmail.com>
2024-09-17 14:18:51 +0100

    receptionist controller...im beginning to overthink this

--- controllers/receptionistController.js
+++ controllers/receptionistController.js
@@ -3,7 +3,7 @@
 const User = require('../models/userModel');
 const Appointment = require('../models/appointmentModel');
 const Patient = require('../models/patientModel');
-
+//To create patient data 
 exports.createPatient = async(req, res) => {
     const { name, cardNumber, age, gender, contact, address } = req.body;
 
@@ -19,3 +19,60 @@ exports.createPatient = async(req, res) => {
     }
 
 }
+
+//To create patient appointment
+exports.createAppointment = async(req, res) => {
+    const { patientId, date, doctor, reason } = req.body;
+    try{
+        const newAppointment = new Appointment({
+            patient: patientId,
+            date,
+            doctor,
+            reason
+        });
+        await newAppointment.save();
+        res.status(200).json({message: 'Appointment booked successfully', appointment: newAppointment});
+    } catch(error) {
+        res.status(500).json({ message: 'Error booking the appointment', error });
+    }
+};
+
+//to edit patient appointment
+exports.editAppointment = async(req, res) => {
+    const { appointmentId, date, doctor, reason } = req.body;
+    try{
+        const updatedAppointment = await Appointment.findByIdAndUpdate(appointmentId,
+            {
+                date, doctor, reason}, { new: true}
+    );
+    if (!updatedAppointment) {
+        return res.status(404).json({ message: 'Appointment not found' })
+    }
+    res.status(200).json({ message: 'Appointment edited successfully', appointment: updatedAppointment});
+    } catch (error) {
+        res.status(500).json({ message: 'Error editing the appointment', error});
+    }
+};
+
+//to delete an appointment
+exports.removeAppointment = async(req, res) => {
+    const { appointmentId } = req.body;
+    try{
+        const deletedAppointment = await Appointment.findByIdAndDelete(appointmentId);
+    if (!deletedAppointment) {
+        return res.status(404).json({ message: 'Appointment not found'});
+    }
+        res.status(200).json({ message: 'Appointment successfully deleted'});
+    } catch (error) {
+        res.status(500).json({ message: 'Error deleting this appointment', error });
+    }
+};
+
+//create or input payment
+exports.createPayment = (req, res) => {
+    const { patientId, paymentMethod, amount, date } = req.body;
+    try {
+        const newPayment = Payment
+    }
+}
+
