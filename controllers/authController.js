ba9a4265805cada77745bc0dd2d9c2ae470ffcc6
ololadetemi <ololadetoluwalase@gmail.com>
2024-09-25 10:13:58 +0100

    creating user authentication and authorization

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,6 @@
 //this controller is for handling authentication purposes. Handling login and later, log out purposes
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+//User login
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator')
+const bcrypt = require('bcryptjs')
 const userSchema = new mongoose.Schema (
     {
         name: {
@@ -7,11 +9,15 @@ const userSchema = new mongoose.Schema (
         },
         email: {
             type: String,
-            required: true
+            required: [true, 'please enter an email'],
+            lowercase: true,
+            unique: true,
+            validate: [isEmail, 'please enter a valid email']
         },
         password: {
             type: String,
-            required: true
+            required: [true, 'please enter a password'],
+            minLength: [6, 'minimum length is 6 characters']
         },
         role: {
             type: String,
@@ -24,6 +30,25 @@ const userSchema = new mongoose.Schema (
         }
     }
 );
+
+//Hashing password before saving to DB
+userSchema.pre('save',async function (next) {
+    if (!this.isModified ('password')) return next();
+//Salting and hashing before saving
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        console.log('User about to be created', this)
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+//to compare inputed password to hashed password
+userSchema.methods.comparePassword= async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+
+};
 
 const User = mongoose.model('user', userSchema);
 
