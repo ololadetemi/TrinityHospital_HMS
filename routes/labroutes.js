ca5576070bcb7e43f12b9ce61d4a1dd65687e0a2
ololadetemi <ololadetoluwalase@gmail.com>
2024-09-25 20:14:01 +0100

    still working

--- routes/labroutes.js
+++ routes/labroutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const labController = require('../controllers/labController');
+const { authenticate, authorize } = require('../middlewares/authMiddleware');
 const upload = require('../config/multerConfig');  // Multer setup
 
 // Route to add lab result with file upload
-router.post('/addResult', upload.single('resultFile'), labController.addLabResult);
+router.post('/addResult', authenticate, authorize('lab_technician'), upload.single('resultFile'), labController.addLabResult);
 
 // Route to edit lab result (if you want file updates, include Multer)
-router.put('/editResult', upload.single('resultFile'), labController.editResult);
+router.put('/editResult/:id', authenticate, authorize('lab_technician'), upload.single('resultFile'), labController.editResult);
 
 module.exports = router;
