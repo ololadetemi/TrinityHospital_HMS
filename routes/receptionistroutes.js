2fc0b66cd7b9edd7d05d9e26dd886567a1c8825c
ololadetemi <ololadetoluwalase@gmail.com>
2024-09-25 20:29:46 +0100

    still working

---
+++ routes/receptionistroutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const { authenticate, authorize } = require('../middlewares/authMiddleware');
+const adminController = require('../controllers/receptionistController');
+
+router.post('/create-patient', authenticate, authorize('receptionist'), receptionistController.createPatient);
+router.post('/create-appointment', authenticate, authorize('receptionist'), receptionistController.createAppointment);
+router.put('/edit-appointment/:id', authenticate, authorize('receptionist'), receptionistController.editAppointment);
+router.delete('/delete-appointment', authenticate, authorize('receptionist'), receptionistController.removeAppointment);
+router.put('/edit-payment/:id', authenticate, authorize('receptionist'), receptionistController.editPayment);
+router.get('/view-appointment', authenticate, authorize('receptionist'), receptionistController.viewAppointments);
+
+module.exports = router;
