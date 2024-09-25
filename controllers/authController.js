a0f4a9a4454d6058f15fbbe9eaca7006d36e52fc
ololadetemi <ololadetoluwalase@gmail.com>
2024-09-25 10:37:45 +0100

    creating auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,5 +2,29 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const { escape } = require('validator');
+require('dotenv').config();
 
 //User login
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+//Check the email address
+        const user = await User.findOne({ email });
+        if(!user) return res.status(401).json({ message: 'Invalid email or password' });
+//Check the password
+        const isMatch = await user.comparePassword(password);
+        if(!isMatch) return res.status(401).json({ message: 'Incorrect password' });
+//Create the jwt token
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+            expiresIn: 3 * 24 * 60 * 60,
+        });
+        res.satus(200).json({ message: 'Login successful', token });
+    } catch (error) {
+        escape.status(500).json({ message: 'Server error', error });
+    };
+};
+
+//To allow admin create new staff only
+exports.register
